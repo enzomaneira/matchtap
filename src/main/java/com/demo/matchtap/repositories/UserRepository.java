fb737517e9e4b015c53c1c32c7bec0516befb7e8
enzomaneira <enzo@maneira.com>
2024-10-01 21:03:40 -0300

    test

--- src/main/java/com/demo/matchtap/repositories/UserRepository.java
+++ src/main/java/com/demo/matchtap/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.demo.matchtap.repositories;
 
-
 import com.demo.matchtap.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
